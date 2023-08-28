b0b5e1547d0d409c8776044e6d44c4e7f0f20298
ariaschecter <aria.maulana.m@gmail.com>
2023-08-28 20:15:14 +0700

    Revert "[PATCH] Frontend Experience Education Project, Project minus Comment"
    This reverts commit 7a45c70c94164706bc8a4dc1e3a7fa79fa7a692c.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EducationController;
 use App\Http\Controllers\ExperienceController;
-use App\Http\Controllers\Home\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\SkillController;
@@ -23,13 +22,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('home.index');
+});
+
 Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
     \UniSharp\LaravelFilemanager\Lfm::routes();
 });
 
-route::controller(HomeController::class)->group( function() {
-    Route::get('/', 'index')->name('home.index');
-});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth', 'verified')->controller(DashboardController::class)->group(function () {
     Route::get('/dashboard', 'index')->name('dashboard');
