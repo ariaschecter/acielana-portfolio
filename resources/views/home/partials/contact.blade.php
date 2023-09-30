812b4845a71bf1c7769b842cd8192c9d673e8ac8
ariaschecter <aria.maulana.m@gmail.com>
2023-09-30 12:00:34 +0700

    [UPDATE] Change Call to WhatsApp

--- resources/views/home/partials/contact.blade.php
+++ resources/views/home/partials/contact.blade.php
@@ -15,7 +15,8 @@
                         Get I contact work
                     </h2>
                     <p class="text-[#636363] dark:text-slate-200 text-[17px] leading-[28px] lg:max-w-[472px] w-full">
-                        Connect for collaboration or inquiries. Excited to hear from you and explore opportunities to work together on creative projects.</p>
+                        Connect for collaboration or inquiries. Excited to hear from you and explore opportunities to
+                        work together on creative projects.</p>
                 </div>
                 <!-- Section title end -->
 
@@ -23,7 +24,8 @@
                 <form action="{{ route('home.contact.store') }}" method="POST">
                     @csrf
                     @if (session()->get('message'))
-                        <p class="text-accent1 text-[20px] lg:text-[24px] font-medium mb-[10px] lg:mb-[5px]">{{ session()->get('message') }}</p>
+                        <p class="text-accent1 text-[20px] lg:text-[24px] font-medium mb-[10px] lg:mb-[5px]">
+                            {{ session()->get('message') }}</p>
                     @endif
                     <div class="grid grid-cols-2 gap-7">
                         <div>
@@ -54,18 +56,21 @@
                     <div>
                         <span
                             class="w-[50px] h-[50px] lg:w-[70px] lg:h-[70px] bg-accent1 text-white flex items-center rounded-full justify-center">
-                            <svg width="25" height="34" fill="currentColor" stroke="currentColor"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                <path
-                                    d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                width="35px" height="35px" viewBox="0 0 30 30" version="1.1">
+                                <g id="surface1">
+                                    <path
+                                        style=" stroke:none;fill-rule:evenodd;fill:rgb(100%,100%,100%);fill-opacity:1;"
+                                        d="M 4.375 15 C 4.375 9.132812 9.132812 4.375 15 4.375 C 20.867188 4.375 25.625 9.132812 25.625 15 C 25.625 20.867188 20.867188 25.625 15 25.625 C 12.910156 25.625 10.964844 25.023438 9.324219 23.984375 C 9.015625 23.789062 8.644531 23.738281 8.296875 23.839844 L 4.695312 24.914062 L 6.050781 21.742188 C 6.210938 21.371094 6.179688 20.941406 5.964844 20.59375 C 4.957031 18.96875 4.375 17.054688 4.375 15 Z M 15 1.875 C 7.75 1.875 1.875 7.75 1.875 15 C 1.875 17.296875 2.464844 19.460938 3.503906 21.339844 L 1.351562 26.382812 C 1.160156 26.828125 1.246094 27.34375 1.566406 27.707031 C 1.890625 28.070312 2.394531 28.210938 2.859375 28.074219 L 8.480469 26.394531 C 10.402344 27.496094 12.628906 28.125 15 28.125 C 22.25 28.125 28.125 22.25 28.125 15 C 28.125 7.75 22.25 1.875 15 1.875 Z M 17.867188 17.726562 L 16.222656 18.886719 C 15.453125 18.445312 14.601562 17.835938 13.75 16.980469 C 12.863281 16.09375 12.203125 15.179688 11.71875 14.339844 L 12.765625 13.453125 C 13.210938 13.074219 13.335938 12.433594 13.058594 11.914062 L 11.730469 9.414062 C 11.550781 9.078125 11.226562 8.839844 10.851562 8.769531 C 10.476562 8.703125 10.089844 8.808594 9.804688 9.058594 L 9.410156 9.402344 C 8.460938 10.230469 7.898438 11.589844 8.363281 12.96875 C 8.847656 14.394531 9.875 16.640625 11.980469 18.75 C 14.25 21.015625 16.550781 21.910156 17.898438 22.257812 C 18.984375 22.535156 20.011719 22.160156 20.734375 21.570312 L 21.472656 20.96875 C 21.789062 20.710938 21.960938 20.316406 21.929688 19.910156 C 21.898438 19.5 21.671875 19.136719 21.324219 18.925781 L 19.226562 17.675781 C 18.800781 17.421875 18.269531 17.445312 17.867188 17.726562 Z M 17.867188 17.726562 " />
+                                </g>
                             </svg>
                         </span>
                     </div>
                     <div class="ltr:pl-6 rtl:pr-6">
                         <h3 class="text-[28px] md:text-[22px] font-heebo font-bold text-primary dark:text-white">
-                            Call Me</h3>
+                            WhatsApp</h3>
                         <span class="text-primary dark:text-slate-200 text-[22px] md:text-[18px] mt-5">
-                            <a href="tel:+6285156631578">+6285156631578</a>
+                            <a href="https://wa.me/6285156631578" target="_blank">6285156631578</a>
                         </span>
                     </div>
                 </div>
@@ -87,7 +92,7 @@
                     </div>
                     <div class="ltr:pl-6 rtl:pr-6">
                         <h3 class="text-[28px] md:text-[22px] font-heebo font-bold text-primary dark:text-white">
-                            Email Us</h3>
+                            Email</h3>
                         <span class="text-primary  dark:text-slate-200 text-[22px] md:text-[18px] mt-5">
                             <a href="mailto:ariaschecter@webmail.umm.ac.id">ariaschecter@webmail.umm.ac.id</a>
                         </span>
